e978fbe15ed397bbc118b59097665b763530d244
Kurt Obando <codeanywhere@panel.cityescape.ph>
2019-03-13 03:35:34 +0000

    set the base url to dynamic

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://dev.kurtobando.com/tools/';
+
+
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$config['base_url']    = "$root"; 
+// https://gist.github.com/ebinnion/5685188 | how to set base url to dynamic
+// $config['base_url'] = 'http://dev.kurtobando.com/tools/';
 
 /*
 |--------------------------------------------------------------------------
