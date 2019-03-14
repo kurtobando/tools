cd4285ee9c436a44ea265e4ebd3e58188d55ca30
Kurt Obando <codeanywhere@panel.cityescape.ph>
2019-03-14 04:30:25 +0000

    add favicon, and set ajax request for both whois and dig records

--- application/views/index.php
+++ application/views/index.php
@@ -10,6 +10,11 @@
 	<meta name="description" content="Custom Tools for Web Troubleshooting for Domains/Hosting">
 	<meta name="keywords" content="toos, web hosting, domain name, dig, whois, myipaddress">
 	
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="assets/favicon/site.webmanifest">
+	
 	<!-- UIkit CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" type="text/css">
@@ -38,9 +43,9 @@
 		</div>
 		
 		
-		<form action="" method="get">
+		<form action="" method="get" name="domainQueryForm">
 			<div class="uk-flex uk-flex-middle">
-				<input class="uk-input" type="text" name="domain-query" value ="<?php echo $this->input->get("domain-query"); ?>"placeholder="e.g. gmail.com, www.gmail.com, mail.google.com" autofocus autocomplete>
+				<input class="uk-input" type="text" name="domainQueryInput" value ="<?php echo $this->input->get("domain-query"); ?>"placeholder="e.g. gmail.com, www.gmail.com, mail.google.com" autofocus autocomplete>
 				<input class="uk-button uk-button-primary" type="submit" value="Dig Records">
 			</div>
 			
@@ -51,150 +56,10 @@
 	
 <!-- 		<label uk-icon="copy">Copy </label> -->
 		<pre class="uk-background-muted">
-			<?php
-
-			// store dns records in
-			$dig_result = array();
-
-
-			$record_NS 		= array();
-			$record_A 		= array();
-			$record_AAAA 	= array();
-			$record_TXT 	= array();
-			$record_CNAME 	= array();	
-			$record_CNAME_values = array();	
-			$record_MX 		= array();
-			$record_MX_values = array();
-
-
-			// prevent empty queries
-			if(!empty($this->input->get("domain-query"))) :
-
-				// http://php.net/manual/en/function.dns-get-record.php
-				// add . to read subdomains records
-				$query = trim($this->input->get("domain-query")) . ".";
-
-				$result = dns_get_record($query, DNS_ALL);
-				// print_r($result);
-
-				foreach ($result as $key => $val){
-
-					// get all NS
-					if($result[$key]['type'] === "NS"){
-						array_push(
-							$record_NS,
-							$val['target']
-						);
-
-						$dig_result["NS"] = $record_NS;
-					}
-
-
-					// get all A
-					if($result[$key]['type'] === "A"){
-						array_push(
-							$record_A,
-							$val['ip']
-						);
-
-						$dig_result["A"] = $record_A;
-					}
-
-
-					// get all CNAME
-					if($result[$key]['type'] === "CNAME"){
-
-						// check MX IPs only for A and AAAA
-						$CNAME_values = dns_get_record($val['target'], DNS_A + DNS_AAAA);
-
-						foreach($CNAME_values as $index => $value ){
-
-							$ipv4 = isset($CNAME_values[0]['ip']) ? $CNAME_values[0]['ip'] : "";
-							$ipv6 = isset($CNAME_values[1]['ipv6']) ? $CNAME_values[1]['ipv6'] : "";
-
-							$record_CNAME_values = array(
-								$val['target'],
-								$ipv4, 
-								$ipv6
-							);
-						}
-
-						array_push(
-							$record_CNAME,
-							$record_CNAME_values
-						);
-
-						$dig_result["CNAME"] = $record_CNAME;
-					}
-
-
-					// get all AAAA
-					if($result[$key]['type'] === "AAAA"){
-						array_push(
-							$record_AAAA,
-							$val['ipv6']
-						);
-
-						$dig_result["AAAA"] = $record_AAAA;
-					}
-
-
-					// get all TXT
-					if($result[$key]['type'] === "TXT"){
-						array_push(
-							$record_TXT,
-							$val['txt']
-						);
-
-						$dig_result["TXT"] = $record_TXT;
-					}
-
-
-					// get all MX
-					if($result[$key]['type'] === "MX"){
-
-						// check MX IPs only for A and AAAA
-						$MX_values = dns_get_record($val['target'], DNS_A + DNS_AAAA);
-
-						foreach($MX_values as $index => $value ){
-
-							$ipv4 = isset($MX_values[0]['ip']) ? $MX_values[0]['ip'] : "";
-							$ipv6 = isset($MX_values[1]['ipv6']) ? $MX_values[1]['ipv6'] : "";
-
-							$record_MX_values = array(
-								$val['target'],
-								$ipv4, 
-								$ipv6
-							);
-						}
-
-						array_push(
-							$record_MX,
-							$record_MX_values
-						);
-
-						$dig_result["MX"] = $record_MX;
-					}
-				}
-
-
-				print_r($dig_result);
-
-				print "<hr>";
-
-
-				// whois query
-				$whois = shell_exec("whois " . $query);
-
-				print_r($whois);
-				
-				echo '<a class="uk-button uk-button-primary uk-float-right" href="#scroll-up" uk-scroll>Scroll Up</a>';
-			endif;
-			
-			
-	
-			?>
+			<ul id="dns-results"></ul>
+			<div id="whois-results"></div>
 		</pre>
+		<a class="uk-button uk-button-primary uk-float-right" href="#scroll-up" uk-scroll>Scroll Up</a>
 	</div>
 	
 	
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,110 @@
+// for screen sizes
 var screen_width = document.getElementById("screen-width");
 var screen_height = document.getElementById("screen-height");
 
 screen_width.innerHTML = window.screen.width;
 screen_height.innerHTML = window.screen.height;
+
+
+// for domain query form
+let xmlhr = new XMLHttpRequest();
+
+let domainQueryForm = document.domainQueryForm;
+let domainQueryInput = domainQueryForm.domainQueryInput;
+let tag_ul_dns = document.getElementById("dns-results");
+let tag_li = document.createElement("li");
+let whois = document.getElementById("whois-results");
+let done_parse;
+
+
+	domainQueryForm.addEventListener('submit', function(event){
+
+		console.info(xmlhr);
+		console.info(event);
+		
+		// prevent default submit in form
+		event.preventDefault();
+		
+		// prevent empty search
+		if(domainQueryInput.value.trim() === ""){
+			console.info("->empty domain or query ip");
+			return false;
+		}
+
+		// send get request
+		xmlhr.open('GET','//dev.kurtobando.com/tools/tools/dns_ajax?domain-query=' + domainQueryInput.value, true);
+		xmlhr.setRequestHeader("X-Requested-With",'xmlhttprequest');
+		
+		// readyState will be 1
+		xmlhr.onloadstart = function () {
+			console.info("->onloadstart");
+			console.log('status:', xmlhr.status);
+			console.log('readyState:', xmlhr.readyState);
+			
+			tag_ul_dns.innerHTML = "loading ...";
+		};
+		
+		// readyState will be 3
+		xmlhr.onprogress = function () {
+			console.info("->onprogress");
+			console.log('status:', xmlhr.status);
+			console.log('readyState:', xmlhr.readyState);
+			
+			tag_ul_dns.innerHTML = "almost there ...";
+		};
+		
+		// readyState will be 4
+		xmlhr.onload = function () {
+			console.info("->onload");
+			console.log('status:', xmlhr.status);
+			console.log('readyState:', xmlhr.readyState);
+			
+			// clear loading text 
+			tag_ul_dns.innerHTML = "";
+			
+			// parse JSON 
+			done_parse = JSON.parse(xmlhr.responseText);
+			
+			// display records to dns-results div
+			display_to_html(done_parse.NS, "NS");
+			display_to_html(done_parse.CNAME, "CNAME");
+			display_to_html(done_parse.AAAA, "AAAA");
+			display_to_html(done_parse.A, "A");
+			display_to_html(done_parse.MX, "MX (w/ corresponding Mail A record)");
+			display_to_html(done_parse.TXT, "TXT");
+			
+			// display whois
+			if(done_parse.whois !== undefined){
+				whois.innerHTML = done_parse.whois;
+			}
+		};
+		
+		xmlhr.send();
+		
+	});
+	
+
+	// https://zellwk.com/blog/looping-through-js-objects/
+	function display_to_html(json, record_name){
+		
+		if(typeof json === "object"){
+			
+			let temp_ul = document.createElement("ul");
+			
+			// insert records name here for example (A, NS, MX and TXT)
+			let temp_li_record_name = document.createElement("li");
+				temp_li_record_name.innerHTML = record_name;
+				temp_ul.appendChild(temp_li_record_name);
+
+			// insert each dns to li then add the temp ul tag
+			for (const [key, value] of Object.entries(json)) {
+				
+				let temp_li_value = document.createElement("li");
+					temp_li_value.innerHTML = value;
+
+					temp_ul.appendChild(temp_li_value);
+					tag_ul_dns.appendChild(temp_ul);
+			}
+		}
+	}
+
