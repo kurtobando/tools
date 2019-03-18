855979e0d2454e32cd43c92103ea33d4c2725a54
Kurt Obando <codeanywhere@panel.cityescape.ph>
2019-03-18 08:08:39 +0000

    move base url to https

--- application/views/index.php
+++ application/views/index.php
@@ -10,10 +10,10 @@
 	<meta name="description" content="Custom Tools for Web Troubleshooting for Domains/Hosting">
 	<meta name="keywords" content="toos, web hosting, domain name, dig, whois, myipaddress">
 	
-	<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-	<link rel="manifest" href="assets/favicon/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url();?>assets/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url();?>assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo base_url();?>assets/favicon/site.webmanifest">
 	
 	<!-- UIkit CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
