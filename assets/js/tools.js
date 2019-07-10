92c8b3cce6e8b64f837ce38b7f792d985eecfe68
Kurt Obando <kurtobando.developer@gmail.com>
2019-07-11 01:48:14 +0800

    major commit - remove codeigniter and create a simple class instead.

---
+++ assets/js/tools.js
@@ -0,0 +1,199 @@
+let dig_form = document.getElementById('dig_form');
+let dig_result = document.getElementById('dig_result');
+let xmlhr = new XMLHttpRequest();
+
+
+
+
+// execute on submit
+dig_form.addEventListener('submit', function(event) {
+
+	event.preventDefault();
+
+	// filter if search if empty string
+	if (dig_form.search.value.trim() !== '') {
+
+		// display pre tag 
+		dig_result.setAttribute('class', '');
+
+		// call XMLHttpRequest
+		callTools(dig_form.search.value);
+
+		return 0;
+	}
+
+	// hide pre tag 
+	dig_result.setAttribute('class', 'uk-invisible');
+
+	return 0;
+
+});
+
+
+
+// execute XMLHttpRequest with 'domains' as the search query
+function callTools(domain) {
+
+	xmlhr.open('GET', 'php/Tools.php?search=' + domain, true);
+	xmlhr.setRequestHeader("X-Requested-With", 'xmlhttprequest');
+	xmlhr.send();
+
+	xmlhr.onloadstart = function() {
+
+		dig_result.innerHTML = '<div uk-spinner></div>';
+	}
+
+	xmlhr.onloadend = function() {}
+
+	xmlhr.onload = function() {
+
+		dig_result.innerHTML = '<p>Records for: ' + domain + '</p>';
+
+		parseTools(JSON.parse(xmlhr.responseText));
+	}
+
+	xmlhr.onprogress = function() {
+
+		dig_result.innerHTML = 'loading ...';
+	}
+
+	xmlhr.onerror = function(e) {
+
+		dig_result.innerHTML = e;
+
+		console.error(e);
+	}
+
+}
+
+
+
+// prepare 'xmlhr_response' result show in HTML
+function parseTools(xmlhr_response) {
+
+	// 	console.log( xmlhr_response.WHOIS );
+	// 	console.log( xmlhr_response.DIG );
+	// 	console.log( getValueOf( xmlhr_response.DIG.A, 'ip' ) );
+	// 	console.log( getValueOf( xmlhr_response.DIG.AAAA, 'ipv6' ) );
+	// 	console.log( getValueOf( xmlhr_response.DIG.MX, 'target' ) );
+	// 	console.log( getValueOf( xmlhr_response.DIG.MX_A, 'ip' ) );
+	// 	console.log( getValueOf( xmlhr_response.DIG.MX_A, 'ipv6' ) );
+	// 	console.log( getValueOf( xmlhr_response.DIG.TXT, 'txt' ) );
+
+	// error handling
+	if (xmlhr_response.WHOIS == 'No whois server is known for this kind of object.\n') {
+
+		dig_result.innerHTML = xmlhr_response.WHOIS;
+
+		return 0;
+
+	}
+
+	// NS record
+	dislayArrayPerLine(getValueOf(xmlhr_response.DIG.NS, 'target'), 'NS RECORD');
+
+	// A record
+	dislayArrayPerLine(getValueOf(xmlhr_response.DIG.A, 'ip'), 'A RECORD');
+	
+	// AAAA record
+	dislayArrayPerLine(getValueOf(xmlhr_response.DIG.AAAA, 'ipv6'), 'AAAA RECORD');
+	
+	// show MX and Mail A + AAAA(if any) record
+	let obj_ = xmlhr_response.DIG.MX_A;
+	
+	if (obj_ !== undefined){
+		dig_result.appendChild(createElement('MX RECORD', 'b'));
+	
+		Object.keys( obj_ ).map( indexOfArray => {
+			
+			dig_result.appendChild(createElement(obj_[indexOfArray][0].host));
+			
+			if (obj_[indexOfArray][0]){
+				dig_result.appendChild(createElement(obj_[indexOfArray][0].ip, 'span'));
+			}
+			
+			if(obj_[indexOfArray][1]){
+				dig_result.appendChild(createElement('	' + obj_[indexOfArray][1].ipv6, 'span'));
+			}
+			
+		});
+	}
+	
+	// TXT record
+	dislayArrayPerLine(getValueOf(xmlhr_response.DIG.TXT, 'txt'), 'TXT RECORD');
+
+	// Whois record
+	dig_result.appendChild(createElement('WHOIS', 'b'));
+	dig_result.appendChild(createElement(xmlhr_response.WHOIS));
+
+}
+
+
+
+// create HTML element
+// 'content' is the  desired content of an the default element
+// 'el' is (default) HTML element
+function createElement(content, el = 'div') {
+
+	let element = document.createElement(el);
+	let text = document.createTextNode(content);
+
+	if (content !== undefined) {
+
+		element.appendChild(text);
+
+		return element;
+	}
+
+	return 0;
+
+}
+
+
+
+// loop array from raw json and return value of "shouldReturnValue"
+// for example: getValueOf(object, 'ip');
+// return value of key 'ip' from object
+function getValueOf(obj, shouldReturnValue) {
+	
+	if (obj.length == 0) {
+
+		return 0;
+	}
+
+	let array = Object.keys(obj).map(indexOfArray => {
+
+		return obj[indexOfArray];
+	});
+
+	let object = Object.keys(array).map(indexOfObject => {
+
+		if (array[indexOfObject][shouldReturnValue] !== undefined) {
+
+			return array[indexOfObject][shouldReturnValue];
+		}
+
+		return 0;
+	});
+
+	return object;
+
+}
+
+
+
+function dislayArrayPerLine(anArray, headline) {
+
+	if (anArray.length == undefined) {
+
+		return 0;
+	}
+
+	dig_result.appendChild(createElement(headline, 'b'));
+
+	for (let item in anArray) {
+
+		dig_result.appendChild(createElement(anArray[item]));
+	}
+	
+}
---
+++ php/Tools.php
@@ -0,0 +1,105 @@
+<?php
+
+error_reporting(0);
+
+class Tools{
+	
+	// https://www.php.net/manual/en/function.dns-get-record.php
+	
+	private $domain;
+	private $result;
+	
+	
+	public function set_domain( $domain = ''){
+		
+		if(!empty( $domain )){
+			return $this->domain =  htmlentities(trim( $domain )) . ".";
+		}
+		
+		exit('exit');
+	}
+	
+	
+	private function __domain_whois(){
+		return $this->result['WHOIS'] = shell_exec("whois " . $this->domain);
+	}
+	
+	
+	private function __domain_dig_a(){
+		return $this->result['DIG']['A'] = dns_get_record( $this->domain, DNS_A);
+	}
+	
+	
+	private function __domain_dig_aaaa(){
+		return $this->result['DIG']['AAAA'] = dns_get_record( $this->domain, DNS_AAAA);
+	}
+	
+	
+	private function __domain_dig_cname(){
+		return $this->result['DIG']['CNAME'] = dns_get_record( $this->domain, DNS_CNAME);
+	}
+	
+	
+	private function __domain_dig_mx(){
+		return $this->result['DIG']['MX'] = dns_get_record( $this->domain, DNS_MX);
+	}
+	
+	
+	private function __domain_dig_mx_a(){
+		
+		$mx_i = 0;
+		$mx_records = $this->result['DIG']['MX'];
+		$mx_records_count = count($this->result['DIG']['MX']);
+		
+		foreach($mx_records as $mx){
+			$this->result['DIG']['MX_A'][$mx_i] = dns_get_record( $mx['target'] . '.', DNS_A + DNS_AAAA);
+			$mx_i++;
+		}
+		
+		return $this->result['DIG']['MX_A'];
+	}
+	
+	
+	private function __domain_dig_ns(){
+		return $this->result['DIG']['NS'] = dns_get_record( $this->domain, DNS_NS);
+	}
+	
+	
+	private function __domain_dig_txt(){
+		return $this->result['DIG']['TXT'] = dns_get_record( $this->domain, DNS_TXT);
+	}
+	
+	
+	private function __domain_dig_soa(){
+		return $this->result['DIG']['SOA'] = dns_get_record( $this->domain, DNS_SOA);
+	}
+	
+	
+	private function __domain_dig_srv(){
+		return $this->result['DIG']['SRV'] = dns_get_record( $this->domain, DNS_SRV);
+	}
+	
+	
+	public function get_result(){
+		
+		$this->__domain_whois();
+		$this->__domain_dig_a();
+		$this->__domain_dig_aaaa();
+		$this->__domain_dig_cname();
+		$this->__domain_dig_mx();
+		$this->__domain_dig_mx_a();
+		$this->__domain_dig_ns();
+		$this->__domain_dig_txt();
+		$this->__domain_dig_soa();
+		$this->__domain_dig_srv();
+		
+		echo json_encode($this->result);
+	}
+	
+}
+
+$Tools = new Tools();
+$Tools->set_domain( $_GET['search'] );
+$Tools->get_result();
+
+?>
